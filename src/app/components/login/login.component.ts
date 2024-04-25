2dcabfa4264891f3fe9770444bf9de3ebe6b6d23
D'Angello <dangelloa.garcian@uqvirtual.edu.co>
2024-04-24 19:24:05 -0500

    carpetas

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { LoginDTO } from '../../dto/login-dto';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginDTO: LoginDTO;
 
+  constructor() {
+    this.loginDTO = new LoginDTO();
+  }
+
+  public login() {
+    console.log(this.loginDTO);
+  }
 }
