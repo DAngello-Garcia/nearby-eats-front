3bc3332d0df180911ea8ae240e4f7ce1adfc6fd5
ATOMOSX <juand.lopezm@uqvirtual.edu.co>
2024-04-27 16:58:53 -0500

    ultimas modificaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { EmailRecuperacionComponent } from './components/email-recuperacion/email-recuperacion.component';
+import { CambiarContraseniaComponent } from './components/cambiar-contrasenia/cambiar-contrasenia.component';
 
 export const routes: Routes = [
     { path: '', component: InicioComponent },
     { path: 'login', component: LoginComponent },
     { path: 'registro', component: RegistroComponent },
     { path: 'email-recuperacion', component: EmailRecuperacionComponent},
+    { path: 'cambiar-contrasenia', component: CambiarContraseniaComponent},
     { path: "**", pathMatch: "full", redirectTo: "" }
 ];
--- src/app/components/cambiar-contrasenia/cambiar-contrasenia.component.ts
+++ src/app/components/cambiar-contrasenia/cambiar-contrasenia.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { UserChangePasswordDTO } from '../../dto/user-change-password-dto';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cambiar-contrasenia',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cambiar-contrasenia.component.html',
   styleUrl: './cambiar-contrasenia.component.css'
 })
 export class CambiarContraseniaComponent {
 
+  userChangePassword: UserChangePasswordDTO
+
+  constructor() {
+    this.userChangePassword = new UserChangePasswordDTO();
+  }
+
+  public changePassword() {
+    console.log(this.userChangePassword);
+  }
+
+  public theAreEqual(): boolean {
+    return this.userChangePassword.newPassword == 
+    this.userChangePassword.confirmPassword;
+  }
 }
