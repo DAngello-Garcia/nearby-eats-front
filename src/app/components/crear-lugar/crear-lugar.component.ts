f40b980e5cb9a282e48c56ac414f626175305044
ATOMOSX <juand.lopezm@uqvirtual.edu.co>
2024-04-30 15:51:36 -0500

    se avanza con la creación del lugar

--- src/app/components/crear-lugar/crear-lugar.component.ts
+++ src/app/components/crear-lugar/crear-lugar.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PlaceCreateDTO } from '../../dto/place-create-dto';
 
 @Component({
   selector: 'app-crear-lugar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './crear-lugar.component.html',
   styleUrl: './crear-lugar.component.css'
 })
 export class CrearLugarComponent {
 
+  placeCreateDTO: PlaceCreateDTO;
+
+  constructor() {
+    this.placeCreateDTO = new PlaceCreateDTO();
+  }
+
+  public createPlace() {
+    console.log(this.placeCreateDTO);
+  }
 }
