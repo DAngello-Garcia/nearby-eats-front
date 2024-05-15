5d5397ba413d76b90f7e1116f05e4e20acc2f9c8
D'Angello <dangelloa.garcian@uqvirtual.edu.co>
2024-05-15 12:05:14 -0500

    sin error SessionStorage not defined

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { usuarioInterceptor } from './interceptor/usuario.interceptor';
+// import { usuarioInterceptor } from './interceptor/usuario.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withInterceptors([usuarioInterceptor]))]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient()]
+  // providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withInterceptors([usuarioInterceptor]))]
 };
