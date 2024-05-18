2a27a692ae4d250f33c3566689b011d8876c4453
ATOMOSX <juand.lopezm@uqvirtual.edu.co>
2024-05-18 11:59:43 -0500

    Se organiza actualizar cuenta

--- src/app/components/actualizar-cuenta/actualizar-cuenta.component.ts
+++ src/app/components/actualizar-cuenta/actualizar-cuenta.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UserUpdateDTO } from '../../dto/user/user-update-dto';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { UserInformationDTO } from '../../dto/user/user-information-dto';
+import { ImageServiceService } from '../../services/controllers/image-service.service';
+import { Alert } from '../../dto/clases/alert';
+import { UserServiceService } from '../../services/controllers/user-service.service';
+import { error } from 'console';
 
 @Component({
   selector: 'app-actualizar-cuenta',
@@ -11,21 +17,41 @@ import { RouterLink } from '@angular/router';
   templateUrl: './actualizar-cuenta.component.html',
   styleUrl: './actualizar-cuenta.component.css'
 })
-export class ActualizarCuentaComponent {
+export class ActualizarCuentaComponent implements OnInit {
 
   userUpdateDTO: UserUpdateDTO;
   citys: string[];
   archivos!: FileList;
+  client: UserInformationDTO;
+  alert!: Alert
 
-  constructor() {
+  constructor(
+    private userDataService: UserService,
+    private imageService: ImageServiceService,
+    private userService: UserServiceService ) {
+    this.client = new UserInformationDTO();
     this.userUpdateDTO = new UserUpdateDTO();
     this.citys = [];
+  }
+
+  ngOnInit(): void {
     this.uploadCitys();
+    this.userDataService.getUser();
+
   }
 
   public updateAccount() {
     if (this.userUpdateDTO.profilePicture != "") {
-      console.log(this.userUpdateDTO);
+
+      this.userService.updateUserUSer(this.userUpdateDTO).subscribe({
+        next: (data) => {
+          this.alert = new Alert(data.response, "success");
+        },
+        error: (error) => {
+          this.alert = new Alert(error.error.response, "danger")
+        }
+      });
+      
     } else {
       console.log("Debe cargar una foto");
     }
@@ -39,6 +65,29 @@ export class ActualizarCuentaComponent {
     if (event.target.files.length > 0) {
       this.archivos = event.target.files;
       this.userUpdateDTO.profilePicture = this.archivos[0].name;
+    }
+  }
+
+  
+  public uploadImage() {
+
+    if (this.archivos != null && this.archivos.length > 0) {
+
+      const formData = new FormData();
+      formData.append('file', this.archivos[0]);
+
+      this.imageService.uploadImage(formData).subscribe({
+        next: data => {
+          this.userUpdateDTO.profilePicture = data.response.url;
+          this.alert = new Alert("Se ha subido la foto", "success");
+        },
+        error: error => {
+          this.alert = new Alert(error.error, "danger");
+        }
+      });
+
+    } else {
+      this.alert = new Alert("Debe seleccionar una imagen y subirla", "danger");
     }
   }
 }
