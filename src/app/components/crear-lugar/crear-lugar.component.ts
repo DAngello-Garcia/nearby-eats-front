81c799428b326e1c1f1072439f7bc8ca97e23fc4
ijuanvl19 <juanp.velezl19@gmail.com>
2024-05-18 18:13:35 -0500

    added create place page

--- src/app/components/crear-lugar/crear-lugar.component.ts
+++ src/app/components/crear-lugar/crear-lugar.component.ts
@@ -6,12 +6,10 @@ import { CommonModule } from '@angular/common';
 import { Schedule } from '../../dto/clases/schedule';
 import { MapaService } from '../../services/mapa.service';
 import { PublicServiceService } from '../../services/controllers/public.service';
-import { error } from 'console';
 import { PlaceServiceService } from '../../services/controllers/place-service.service';
 import { Alert } from '../../dto/clases/alert';
 import { ImageServiceService } from '../../services/controllers/image-service.service';
 import { AlertComponent } from '../alert/alert.component';
-import { response } from 'express';
 import { TokenService } from '../../services/token.service';
 
 @Component({
@@ -19,31 +17,38 @@ import { TokenService } from '../../services/token.service';
   standalone: true,
   imports: [FormsModule, CommonModule, RouterLink, AlertComponent],
   templateUrl: './crear-lugar.component.html',
-  styleUrl: './crear-lugar.component.css'
+  styleUrl: './crear-lugar.component.css',
 })
 export class CrearLugarComponent implements OnInit {
-
+  // FORM
   placeCreateDTO: PlaceCreateDTO;
-  archivos!: FileList;
-  schedules: Schedule[];
-  categories: string[];
-  phones: string[];
+  files!: FileList;
+  currentSchedule: Schedule;
+  currentCategory: string;
+  currentPhone: string;
+
+  // ALERT
   alert!: Alert;
-  currentCategory: string
+
+  // THE SELECT INPUT DATA
+  categories: string[];
+  weekdays: string[];
 
   constructor(
     private placeService: PlaceServiceService,
+    private tokenService: TokenService,
     private mapService: MapaService,
     private publicService: PublicServiceService,
-    private imageService: ImageServiceService,
-    private tokenService: TokenService) {
-
-    this.phones = [''];
+    private imageService: ImageServiceService
+  ) {
     this.placeCreateDTO = new PlaceCreateDTO();
+    this.currentCategory = '';
+    this.currentPhone = '';
+    this.currentSchedule = new Schedule('', '', '');
     this.categories = [];
-    this.currentCategory = ''
-    this.schedules = [new Schedule('', '', '')];
-    this.uploadCategories();
+    this.weekdays = [];
+    this.fetchWeekdays();
+    this.fetchCategories();
   }
 
   ngOnInit(): void {
@@ -52,76 +57,142 @@ export class CrearLugarComponent implements OnInit {
     this.mapService.addMarcador().subscribe((marcador) => {
       this.placeCreateDTO.location.coordinates[0] = marcador.lat;
       this.placeCreateDTO.location.coordinates[1] = marcador.lng;
-    })
+    });
   }
 
-  public createPlace() {
-    this.placeService.createPlace(this.placeCreateDTO).subscribe({
-      next: (data) => {
-        this.placeCreateDTO.clientId = this.tokenService.getId();
-        this.alert = new Alert(data.response, "sucess");
-      },
-      error: (error) => {
-        this.alert = new Alert(error.error.response, "danger")
-      }
-    })
+  private fetchWeekdays() {
+    this.weekdays = [
+      'Lunes',
+      'Martes',
+      'Miercoles',
+      'Jueves',
+      'Viernes',
+      'Sabado',
+      'Domingo',
+    ];
   }
 
-  public addSchedule() {
-    this.schedules.push(new Schedule('', '', ''));
-  }
-
-  public addPhones() {
-    this.phones.push('');
-  }
-
-  public onFileChange(event: any) {
-    if (event.target.files.length > 0) {
-      this.archivos = event.target.files;
-      Array.from(this.archivos).forEach(file => { this.placeCreateDTO.images.push(file.name) });
-    }
-  }
-
-  private uploadCategories() {
+  private fetchCategories() {
     this.publicService.getCategories().subscribe({
       next: (data) => {
         this.categories = data.response;
       },
       error: (error) => {
-        console.log("Error al cargar las categorias")
-      }
-    })
+        console.log('Error al cargar las categorias');
+      },
+    });
   }
 
-  public addCategory() {
-    if (this.currentCategory && !this.placeCreateDTO.categories.includes(this.currentCategory)) {
-      this.placeCreateDTO.categories.push(this.currentCategory);
-      this.currentCategory = '';
+  public onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      const files = event.target.files;
+      console.log('this.files', files);
+      this.files = event.target.files;
     }
   }
 
   public uploadImages() {
-
-    if (this.archivos != null && this.archivos.length > 0) {
-
+    if (this.files != null && this.files.length > 0) {
       const formData = new FormData();
 
-      for (let i = 0; i < this.archivos.length; i++) {
-        formData.append('files', this.archivos[i])
+      for (let i = 0; i < this.files.length; i++) {
+        formData.append('files', this.files[i]);
       }
 
       this.imageService.uploadImages(formData).subscribe({
-        next: data => {
-          this.placeCreateDTO.images = data.response.map((response: { url: any; }) => response.url);
-          this.alert = new Alert("Se ha subido la foto", "success");
-        },
-        error: error => {
-          this.alert = new Alert(error.error, "danger");
-        }
-      });
+        next: (data) => {
+          let previousImages = this.placeCreateDTO.images;
+          let newImages = data.response.map(
+            (response: { url: any }) => response.url
+          );
+          this.placeCreateDTO.images = previousImages.concat(newImages);
 
+          this.alert = new Alert('Se ha subido la foto', 'success');
+        },
+        error: (error) => {
+          this.alert = new Alert(error.error, 'danger');
+        },
+      });
     } else {
-      this.alert = new Alert("Debe seleccionar una imagen y subirla", "danger");
+      this.alert = new Alert('Debe seleccionar una imagen y subirla', 'danger');
     }
+  }
+
+  public addCategory() {
+    if (this.currentCategory == '') {
+      this.alert = new Alert('Debe seleccionar una categoria', 'danger');
+      return;
+    }
+
+    this.placeCreateDTO.categories.push(this.currentCategory);
+
+    this.categories = this.categories.filter(
+      (category) => category != this.currentCategory
+    );
+
+    this.currentCategory = '';
+  }
+
+  public removeCategory(category: string) {
+    this.placeCreateDTO.categories = this.placeCreateDTO.categories.filter(
+      (c) => c != category
+    );
+  }
+
+  public addSchedule() {
+    if (this.currentSchedule.weekday == '') {
+      this.alert = new Alert('Debe seleccionar un dia de la semana', 'danger');
+      return;
+    }
+
+    // Agregar el horario actual y eliminar el weekDay
+    this.placeCreateDTO.schedule.push(this.currentSchedule);
+    // Eliminar el weekday
+    this.weekdays = this.weekdays.filter(
+      (weekday) => weekday != this.currentSchedule.weekday
+    );
+    this.currentSchedule = new Schedule('', '', '');
+  }
+
+  public removeSchedule(schedule: Schedule) {
+    this.placeCreateDTO.schedule = this.placeCreateDTO.schedule.filter(
+      (s) => s != schedule
+    );
+  }
+
+  public addPhone() {
+    if (this.currentPhone == '') {
+      this.alert = new Alert('Debe ingresar un numero de telefono', 'danger');
+      return;
+    }
+
+    if (this.placeCreateDTO.phones.includes(this.currentPhone)) {
+      this.alert = new Alert(
+        'El numero de telefono ya esta agregado',
+        'danger'
+      );
+      return;
+    }
+    this.placeCreateDTO.phones.push(this.currentPhone);
+    this.currentPhone = '';
+  }
+
+  public removePhone(phone: string) {
+    this.placeCreateDTO.phones = this.placeCreateDTO.phones.filter(
+      (p) => p != phone
+    );
+  }
+
+  public createPlace() {
+    this.placeCreateDTO.clientId = this.tokenService.getId();
+    console.log('this.placeCreateDTO', this.placeCreateDTO);
+    this.placeService.createPlace(this.placeCreateDTO).subscribe({
+      next: (data) => {
+        this.alert = new Alert(data.response, 'success');
+      },
+      error: (error) => {
+        this.alert = new Alert(error.error.response, 'danger');
+      },
+    });
   }
 }
--- src/app/dto/place/place-create-dto.ts
+++ src/app/dto/place/place-create-dto.ts
@@ -1,16 +1,15 @@
-import { Location } from "../clases/location";
-import { Schedule } from "../clases/schedule";
+import { Location } from '../clases/location';
+import { Schedule } from '../clases/schedule';
 
 export class PlaceCreateDTO {
-
-    constructor(
-        public name: string = '',
-        public description: string = '',
-        public location: Location = new Location('', []),
-        public images: string[] = [],
-        public schedule: Schedule[] = [] ,
-        public phones: string[] = [],
-        public categories: string[] = [],
-        public clientId: string = ''
-    ) {}
+  constructor(
+    public name: string = '',
+    public description: string = '',
+    public location: Location = new Location('location', []),
+    public images: string[] = [],
+    public schedule: Schedule[] = [],
+    public phones: string[] = [],
+    public categories: string[] = [],
+    public clientId: string = ''
+  ) {}
 }
