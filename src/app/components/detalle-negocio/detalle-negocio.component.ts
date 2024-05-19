483d477560a1ff4158b7d8634cec0cbaa0856ea0
ijuanvl19 <juanp.velezl19@gmail.com>
2024-05-18 19:09:05 -0500

    added detail place sample

--- src/app/components/detalle-negocio/detalle-negocio.component.ts
+++ src/app/components/detalle-negocio/detalle-negocio.component.ts
@@ -1,40 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemNegocioDTO } from '../../dto/place/item-negocio-dto';
 import { ActivatedRoute } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { PlaceServiceService } from '../../services/controllers/place-service.service';
-import { MenssageDTO } from '../../dto/menssage-dto';
-
+import { MapaService } from '../../services/mapa.service';
 
 @Component({
   selector: 'app-detalle-negocio',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './detalle-negocio.component.html',
-  styleUrl: './detalle-negocio.component.css'
+  styleUrl: './detalle-negocio.component.css',
 })
-export class DetalleNegocioComponent {
-
+export class DetalleNegocioComponent implements OnInit {
   codePlace: string = '';
   negocio: ItemNegocioDTO;
 
   constructor(
     private route: ActivatedRoute,
-    private placeService: PlaceServiceService
+    private placeService: PlaceServiceService,
+    private mapaService: MapaService
   ) {
-    this.negocio = new ItemNegocioDTO()
+    this.negocio = new ItemNegocioDTO();
     this.route.params.subscribe((params) => {
       this.codePlace = params['id'];
       this.getPlace();
     });
   }
 
-  public getPlace() {
+  ngOnInit(): void {
+    this.mapaService.createMap();
+  }
+
+  private getPlace() {
     this.placeService.getPlace(this.codePlace).subscribe({
-      next: data => {
-        this.negocio = data.response
-      }
+      next: (data) => {
+        this.negocio = data.response;
+        this.mapaService.paintMarcador([this.negocio]);
+      },
     });
+  }
+
+  getDummyReviews() {
+    return [
+      {
+        reviewer: 'John Doe',
+        rating: 4,
+        profileImage: 'https://via.placeholder.com/50',
+      },
+      {
+        reviewer: 'Jane Smith',
+        rating: 5,
+        profileImage: 'https://via.placeholder.com/50',
+      },
+      {
+        reviewer: 'Sam Brown',
+        rating: 3,
+        profileImage: 'https://via.placeholder.com/50',
+      },
+    ];
+  }
+
+  public getStars(rating: number): number[] {
+    return new Array(rating);
   }
 }
