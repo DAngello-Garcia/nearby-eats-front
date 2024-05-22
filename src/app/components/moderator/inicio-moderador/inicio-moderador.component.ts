8255902d86439aa7a5d06ad71ae91c2934c4cf5f
ATOMOSX <juand.lopezm@uqvirtual.edu.co>
2024-05-21 19:02:17 -0500

    Se agregan vistas funcionales del moderador

--- src/app/components/moderator/inicio-moderador/inicio-moderador.component.ts
+++ src/app/components/moderator/inicio-moderador/inicio-moderador.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TokenService } from '../../../services/token.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-moderador',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inicio-moderador.component.html',
   styleUrl: './inicio-moderador.component.css'
 })
--- src/app/components/moderator/solicitudes-pendientes/solicitudes-pendientes.component.ts
+++ src/app/components/moderator/solicitudes-pendientes/solicitudes-pendientes.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { table } from 'console';
+import { PlaceServiceService } from '../../../services/controllers/place-service.service';
+import { ItemNegocioDTO } from '../../../dto/place/item-negocio-dto';
 
 @Component({
   selector: 'app-solicitudes-pendientes',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './solicitudes-pendientes.component.html',
   styleUrl: './solicitudes-pendientes.component.css'
 })
-export class SolicitudesPendientesComponent {
+export class SolicitudesPendientesComponent implements OnInit {
 
+  activeTab: string = 'pendientes';
+  negocios: ItemNegocioDTO[] = [];
+  status: string = 'PENDING';
+
+  constructor(
+    private placeService: PlaceServiceService
+  ) {}
+
+  ngOnInit(): void {
+    this.getPlacesByMod();
+  }
+
+  public getPlacesByMod() {
+
+    this.placeService.getPlacesMod(this.status).subscribe({
+      next: data => {
+        this.negocios = data.response;
+      }
+    })
+  }
+
+  public setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
 }
